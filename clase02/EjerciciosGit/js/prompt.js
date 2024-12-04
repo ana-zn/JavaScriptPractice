6993cabd5c3cf632b1c29ff25ad8ff3906fac672
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-03 21:29:53 -0300

    Ejercicios Prompt

---
+++ clase02/EjerciciosGit/js/prompt.js
@@ -0,0 +1,43 @@
+
+// 1 Pide al usuario que ingrese su nombre y muestra su nombre en la consola.
+let nombre = prompt("Ingresa tu nombre"); 
+console.log("El nombre del usuario es " + nombre); 
+
+//2 Solicita al usuario que ingrese su edad y luego muestra un mensaje en la consola con esa edad.
+let edad = parseInt(prompt("Ingresa tu edad")); 
+console.log("La edad del usuario es: " + edad); 
+
+//3 Pide al usuario que ingrese dos números y muestra la suma de estos números en la consola.
+
+let a = parseInt(prompt("Ingrese un numero: ")); 
+let b = parseInt(prompt("Ingrese un segundo numero: "))
+suma = a + b; 
+console.log("La suma de los numeros ingresados: " + suma); 
+
+//4 Solicita al usuario que ingrese su color favorito y muestra un mensaje con ese color en la consola.
+let color = prompt("Ingrese su color favorito: "); 
+console.log("El color favorito del usuario es: " + color); 
+
+//5 Pide al usuario que ingrese una frase y muestra esa frase en la consola.
+let frase = prompt("Ingrese una frase: "); 
+console.log("Frase: "+ frase); 
+
+//6 Pregunta al usuario su ciudad de nacimiento y muestra un mensaje con esa ciudad en la consola.
+
+let ciudad = prompt("¿En que cuidad vivis?"); 
+console.log("La cuidad en la que vive " + nombre + " es " + ciudad); 
+
+//7 Solicita al usuario que ingrese su película favorita y muestra un mensaje con esa película en la consola. 
+
+let peli = prompt("¿Cual es tu pelicula favorita?"); 
+console.log("El nombre de su pelicula favorita es: " + peli); 
+
+//8 Pide al usuario que ingrese el nombre de su mascota y muestra un mensaje con ese nombre en la consola.
+let mascota = prompt ("¿Como se llama tu mascota?: "); 
+console.log("Su mascota se llama "+ mascota); 
+
+//9 Solicita al usuario que ingrese su deporte favorito y muestra un mensaje con ese deporte en la consola. 
+let sport = prompt("Dime cual es tu deporte favorito: "); 
+console.log("El deporte favorito de  " + nombre + " es " + sport); 
+
+//10 Pide al usuario que ingrese su canción favorita y muestra un mensaje con esa canción en la consola.

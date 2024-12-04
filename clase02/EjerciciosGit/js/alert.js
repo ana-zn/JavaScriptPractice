c2451a22a4d63d093a52edda48fa16b26c02121e
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-03 21:34:07 -0300

    Ejercicios Alert

---
+++ clase02/EjerciciosGit/js/alert.js
@@ -0,0 +1,35 @@
+//1. Muestra un mensaje de bienvenida usando alert.
+let name = prompt("Ingresa tu nombre: ")
+alert("Bienvenida " + name ); 
+
+//2.Pregunta al usuario su comida favorita y luego muestra un mensaje de alerta con esa comida.
+let a = prompt("¿Cual es tu comida favorita?" ); 
+alert("Tu comida favorita es: " + a + " a nosotros tambien nos encanta " ); 
+
+//3. Muestra un mensaje de alerta que diga "¡Buenos días!".
+
+alert("Buenos días " + name); 
+
+//4. Muestra un mensaje de alerta que diga "¡Buenas tardes!".
+
+alert("Buenas tardes " + name); 
+
+//5. Muestra un mensaje de alerta que diga "¡Buenas noches!".
+alert("Buenas noches " + name); 
+
+//6. Pregunta al usuario su animal favorito y muestra un mensaje de alerta con ese animal.
+
+let animal = prompt("¿Cual es tu animal favorito?")
+alert(name + " tu animal favorito es genial, a nosotros nos encantan los " + animal); 
+
+//7. Muestra un mensaje de alerta que diga "¡Feliz cumpleaños!".
+
+alert("¡" + name + " , Feliz Cumpleaños! ")
+
+//8. Pregunta al usuario su bebida favorita y muestra un mensaje de alerta con esa bebida.
+let b = prompt("¿Cual es tu bebida favorita?"); 
+alert("tu bebida favorita es: " + b); 
+
+//9. Pregunta al usuario su estación del año favorita y muestra un mensaje de alerta con esa estación.
+let c = prompt("¿Cual es la estacion favorita?"); 
+alert("tu estacion favortia es " + c); 

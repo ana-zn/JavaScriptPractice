0840d1456302e6c657dffe2bd5c33d4badced6ee
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-10 18:49:33 -0300

    Métodos y Propiedades

---
+++ js/metodosPropiedades.js
@@ -0,0 +1,72 @@
+//Método Push----------------------------------------------
+
+// let frutas = ['manzana', 'banana'];
+// frutas.push('naranja');
+// console.log(frutas);  // Salida: ['manzana', 'banana', 'naranja']
+
+
+//Método Pop----------------------------------------------
+
+// let frutas1 = ['manzana', 'banana', 'naranja'];
+// let frutaEliminada = frutas1.pop();
+// console.log(frutaEliminada);  // Salida: 'naranja'
+// console.log(frutas1);  // Salida: ['manzana', 'banana']
+
+
+//Método Shift---------------------------------------------
+
+// let frutas2 = ['manzana', 'banana', 'naranja'];
+// let primeraFruta = frutas2.shift();
+// console.log(primeraFruta);  // Salida: 'manzana'
+// console.log(frutas2);  // Salida: ['banana', 'naranja']
+
+//Método Unshift-------------------------------------------
+
+
+// let frutas3 = ['banana', 'naranja'];
+// frutas.unshift('manzana');
+// console.log(frutas3);  // Salida: ['manzana', 'banana', 'naranja']
+
+//Método Indexof ------------------------------------------
+
+// let nombres = ['Ana', 'Juan', 'Carlos', 'Marta'];
+// let indice = nombres.indexOf('Carlos');
+// console.log(indice);  // Salida: 2
+
+//Método includes ----------------------------------------
+
+// let frutas4 = ['manzana', 'naranja', 'banana'];
+// let contiene = frutas4.includes('naranja');
+// console.log(contiene);  // Salida: true
+
+
+//Metodo Sort, ordena los elementos de manera unicode 
+
+
+// let num = [10,5, 78, 200,3]
+
+// numeros.sort((a,b) => a - b ); 
+// console.log(numeros); 
+
+
+// let palabra = ["auto", "zorro", "programacion" , "banana"]
+// palabra.sort((a,b)=>a.localeCompare(b)); 
+// console.log(palabra); 
+
+
+//Metodo Rever, invierte 
+// numeros.reverse(); 
+// console.log(numeros); 
+
+
+//Metodo Join 
+//Uso basico con coma 
+let colores = ['Rojo', 'Verde', 'Azul'];
+let resultado = colores.join();
+console.log(resultado);  // "Rojo,Verde,Azul"
+
+//
+
+let nombres = ['Ana', 'Juan', 'Carlos'];
+let listaNombres = nombres.join(' ');
+console.log(listaNombres);  // "Ana Juan Carlos"

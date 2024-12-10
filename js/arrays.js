dfbfe0ea3aebc4ae18faa776c66b683f19330dd9
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-10 18:04:48 -0300

    Fundamentos de Arrays

---
+++ js/arrays.js
@@ -0,0 +1,57 @@
+
+// Array vacío
+const arrayVacio = [];
+
+// Array con números
+const numeros = [1, 2, 3];
+
+// Array con strings
+const colores = ["rojo", "verde", "azul"];
+
+// Array con varios tipos de datos
+const mixto = [1, "dos", true];
+
+const frutas = ["manzana", "banana", "cereza"];
+
+// Acceder al primer elemento
+console.log(frutas[0]);  // "manzana"
+
+// Acceder al tercer elemento
+console.log(frutas[2]);  // "cereza"
+
+
+//Declaracion de Arrays
+
+let numero = [1,2,3,4]
+console.log(numero); 
+
+console.log(numero[3]); //Llama a la posicion 3 de la lista numero 
+
+let palabras = ["auto","manzana", "pera", "programacion"]
+
+console.log(palabras); 
+console.log(palabras[0]); 
+
+let boleanos = [true, false, true, false]; 
+
+console.log(boleanos); //los arrays nos sirve para cualquier tipo de dato 
+
+
+let matriz = [ 
+    [1, 2, 3, 4]
+    [5, 6, 7, 8]
+]
+
+console.log(matriz); 
+
+console.log(matriz [0] [2])
+
+
+// Supongamos que tenemos el siguiente array de números
+const numeros1 = [10, 20, 30, 40, 50];
+
+// Para recorrer este array usaremos un bucle for
+for (let i = 0; i < numeros1.length; i++) {
+    console.log(numeros1[i]); // Esto imprimirá cada número del array en la consola
+}
+

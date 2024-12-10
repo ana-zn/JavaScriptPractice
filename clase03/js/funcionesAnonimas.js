0c9f57b91673ba27c201a248de77c317edc313c9
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-10 16:28:39 -0300

    Funciones Anónimas y Flecha

---
+++ clase03/js/funcionesAnonimas.js
@@ -0,0 +1,32 @@
+
+const ejemplo = function(a, b) {
+    return a + b;
+};
+
+console.log(ejemplo(5, 3)); // Salida: 8
+
+//Sintaxis Tradicional 
+
+// const sumar = function(a, b) {
+//     return a + b;
+// };
+
+//Funcion flecha
+
+// const sumar = (a, b) => a + b;
+// console.log(sumar(5,3))
+
+
+//Funcion tradicional 
+function sumar(a ,b){
+    return a + b;  
+}
+
+console.log(sumar(10,5)); 
+
+//funcion flecha 
+
+const sumaA = (c ,d )=> c + d
+
+console.log(sumaA(20,5)); 
+

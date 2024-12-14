ed984769af96ccde226c13f57d6efceef8a8a292
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-14 00:32:03 -0300

    Ejercicios Git: forloop

---
+++ clase03/ejerciciosgit/js/forloop.js
@@ -0,0 +1,32 @@
+/* 
+
+Crear un bucle for que pida al usuario 5 números y calcule su suma.
+Imprimir los elementos de un array usando un bucle for.*/
+// for(let i  = 0;i <5; i++){
+//     console.log("Me estoy ejecutando " + (i+1) + " veces!")
+// }
+
+//--------------------------------------------- (Completado)
+//1. Imprimir los números del 1 al 10 usando un bucle for.
+for(let i = 0; i<10; i++){
+    console.log(`Me estoy ejecutando ${i} veces`)
+}
+
+//--------------------------------------------- (Completado)
+//2.Sumar los números del 1 al 100 y mostrar el resultado usando un bucle for.
+
+let suma = 0;
+for (let i = 1; i <= 100; i++) {
+  suma += i;
+}
+console.log(suma); 
+
+//3. Imprimir los números pares del 1 al 20 usando un bucle for.
+
+
+//4.Crear un bucle for que pida al usuario 5 números y calcule su suma.
+
+for (let i = 0 ; i<5; i++){
+    let num = parseInt(prompt("Ingrese un numero: "))
+    n
+}

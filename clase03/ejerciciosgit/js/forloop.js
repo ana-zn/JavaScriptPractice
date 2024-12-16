84b357af63970d3bdbea4533d42265337f5ad382
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-16 15:08:36 -0300

    Nuevos cambios de ejercicios git

--- clase03/ejerciciosgit/js/forloop.js
+++ clase03/ejerciciosgit/js/forloop.js
@@ -8,25 +8,34 @@ Imprimir los elementos de un array usando un bucle for.*/
 
 //--------------------------------------------- (Completado)
 //1. Imprimir los números del 1 al 10 usando un bucle for.
-for(let i = 0; i<10; i++){
-    console.log(`Me estoy ejecutando ${i} veces`)
-}
+// for(let i = 0; i<10; i++){
+//     console.log(`Me estoy ejecutando ${i} veces`)
+// }
 
 //--------------------------------------------- (Completado)
 //2.Sumar los números del 1 al 100 y mostrar el resultado usando un bucle for.
 
-let suma = 0;
-for (let i = 1; i <= 100; i++) {
-  suma += i;
-}
-console.log(suma); 
+// let suma = 0;
+// for (let i = 1; i <= 100; i++) {
+//   suma += i;
+// }
+// console.log(suma); 
 
+//--------------------------------------------- (Completado)
 //3. Imprimir los números pares del 1 al 20 usando un bucle for.
+// for (let i = 0; i<20; i++){
+//   if(i%2===0){
+//     console.log(i)
+//   }
+// }
 
-
+//--------------------------------------------- (Completado)
 //4.Crear un bucle for que pida al usuario 5 números y calcule su suma.
+// let suma = 0; 
+// for (let i = 0 ; i<5; i++){
+//     let num = parseInt(prompt("Ingrese un numero: "))
+//     suma += num;
+// }
+// alert(`La suma de todos de los numeros ingresados es ${suma}`)
 
-for (let i = 0 ; i<5; i++){
-    let num = parseInt(prompt("Ingrese un numero: "))
-    n
-}
+//5.Imprimir los elementos de un array usando un bucle for.

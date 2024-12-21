bba3822963fd43d20c1738675b0b3785fd9ad7db
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-21 18:06:59 -0300

    Clase en vivo

---
+++ js/vivo.js
@@ -0,0 +1,14 @@
+//objetos 
+const persona2 ={
+    id: 2, 
+    nombre: "Francisco",
+    apellido: "Giarelli", 
+    edad: 24, 
+    email: "Francisco@gmail.com"
+}
+console.log(persona2)
+
+function Persona(Persona){
+    this.id = Persona.id; 
+    this.nombre = persona.nombre; 
+}

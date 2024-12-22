09e89e634de0a1fc35b8ea2a45c918ae4903e6b4
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-22 17:30:50 -0300

    Ejercicios Git Consola

---
+++ clase01/git/js/consola.js
@@ -0,0 +1,12 @@
+/*Imprime "Hola, mundo" en la consola.
+Imprime tu nombre en la consola.
+Muestra un mensaje de error en la consola.
+Imprime el resultado de 5 + 3 en la consola.
+Imprime el resultado de 10 - 7 en la consola.
+Declara una variable nombre con tu nombre y muéstralo en la consola.
+Declara una variable edad con tu edad y muéstralo en la consola.
+Imprime el resultado de 8 * 5 en la consola.
+Imprime el resultado de 20 / 4 en la consola.
+Declara dos variables a y b con valores numéricos y muestra su suma en la consola.
+Imprime el resultado de la concatenación de dos cadenas de texto.
+ */

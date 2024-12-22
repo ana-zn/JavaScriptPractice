2fa596278d0d5a83da813cfe07067008120be50d
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-22 10:45:45 -0300

    Foorloop git ejercicio

---
+++ git/js/forLoop.js
@@ -0,0 +1,44 @@
+
+//1. Imprimir los números del 1 al 10 usando un bucle for. 
+
+// for (let i=0; i<100; i++){
+//     console.log(i); 
+// }
+
+//2. Sumar los números del 1 al 100 y mostrar el resultado usando un bucle for.
+
+// let suma=0; 
+
+// for (let i=1; i<=100; i++){
+//     suma+=i; 
+//     console.log("La suma de los numeros del 1 a 100 es:", suma); 
+// }
+
+
+//3. Imprimir los números pares del 1 al 20 usando un bucle for.
+
+
+// for (let i = 1 ; i<=20; i++){
+//     console.log(i);
+    
+// }
+
+//4. Crear un bucle for que pida al usuario 5 números y calcule su suma.
+// let suma=0; 
+// for (let i= 0; i<5; i++){
+//     let num = parseInt(prompt("Puedes ingresar el numero "));
+//     suma+=num; 
+// }
+// console.log("La suma de los numeros es " + suma); 
+
+//5. Imprimir los elementos de un array usando un bucle for.
+
+let frutas = [
+    "manzana", 
+    "banana", 
+    "frutilla", 
+    "pera"
+]
+console.log(frutas); 
+
+console.log(frutas[2]); 

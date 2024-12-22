1348d3a01482f2a634441391df4816efa259b42e
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2024-12-22 18:10:08 -0300

    Se agregaron nuevos archivos a la carpeta git

---
+++ clase01/git/js/alert.js
@@ -0,0 +1,12 @@
+/* 
+Muestra un mensaje de bienvenida usando alert.
+Pregunta al usuario su comida favorita y luego muestra un mensaje de alerta con esa comida.
+Muestra un mensaje de alerta que diga "¡Buenos días!".
+Muestra un mensaje de alerta que diga "¡Buenas tardes!".
+Muestra un mensaje de alerta que diga "¡Buenas noches!".
+Pregunta al usuario su animal favorito y muestra un mensaje de alerta con ese animal.
+Muestra un mensaje de alerta que diga "¡Feliz cumpleaños!".
+Pregunta al usuario su bebida favorita y muestra un mensaje de alerta con esa bebida.
+Muestra un mensaje de alerta que diga "¡Feliz año nuevo!".
+Pregunta al usuario su estación del año favorita y muestra un mensaje de alerta con esa estación.
+*/
---
+++ clase01/git/js/prompt.js
@@ -0,0 +1,13 @@
+/*
+Pide al usuario que ingrese su nombre y muestra su nombre en la consola.
+Solicita al usuario que ingrese su edad y luego muestra un mensaje en la consola con esa edad.
+Pide al usuario que ingrese dos números y muestra la suma de estos números en la consola.
+Solicita al usuario que ingrese su color favorito y muestra un mensaje con ese color en la consola.
+Pide al usuario que ingrese una frase y muestra esa frase en la consola.
+Pregunta al usuario su ciudad de nacimiento y muestra un mensaje con esa ciudad en la consola.
+Solicita al usuario que ingrese su película favorita y muestra un mensaje con esa película en la consola.
+Pide al usuario que ingrese el nombre de su mascota y muestra un mensaje con ese nombre en la consola.
+Solicita al usuario que ingrese su deporte favorito y muestra un mensaje con ese deporte en la consola.
+Pide al usuario que ingrese su canción favorita y muestra un mensaje con esa canción en la consola.
+ */
+

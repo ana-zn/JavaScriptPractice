e120bbb5426beae42df99187cef99cb0c83ca93b
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2025-01-16 16:45:51 -0300

    Actualización Recuperar Eliminar

--- js/localStorageRecuperarEliminar.js
+++ js/localStorageRecuperarEliminar.js
@@ -1,3 +1,14 @@
+//RemoveItem- Eliminar datos especificos
+
+// localStorage.removeItem('clave');
+// sessionStorage.removeItem('clave');
+
+// // Clear - Para eliminar todos los daatos 
+
+// localStorage.clear();
+// sessionStorage.clear(); 
+
+
 let nombre = "Raul"; 
 let edad = 40; 
 localStorage.setItem("nombreDeLaPersona", nombre); 

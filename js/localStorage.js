5fc3f4e8a08ecae0776d0eb871ebdb92c5663e93
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2025-01-16 16:22:06 -0300

    ActualizacIón Local Storage

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,4 +1,4 @@
-
+//LOCAL STORAGE 
 // Guardar datos
 localStorage.setItem('usuario', 'JuanPerez');
 // Obtener datos
@@ -7,7 +7,20 @@ console.log(usuario); // 'JuanPerez'
 // Eliminar datos
 localStorage.removeItem('usuario');
 
+//Uso básico de setlem 
 
+//Almacenar cadenas de texto 
+localStorage.setItem('saludo', 'Hola, mundo!');
+
+//Almacenar números (Aunque solo puede almacenar caenas de texto, se puede guardar numeros como texto )
+localStorage.setItem('puntuacion', '10');
+let puntuacion = parseInt(localStorage.getItem('puntuacion')); console.log(puntuacion); // 10 
+//Almacenar datos complejos (como objetos o arrays)
+let usuario1 = { nombre: 'Ana', edad: 25 }; localStorage.setItem('usuario1', JSON.stringify(usuario1));
+
+//Para recuperarlo usar 
+let usuarioRecuperado = JSON.parse(localStorage.getItem('usuario1')); console.log(usuarioRecuperado.nombre); // Ana 
+//SESION STORAGE 
 // Guardar datos
 sessionStorage.setItem('detalleCompra', 'Libro de JavaScript');
 // Obtener datos
@@ -15,3 +28,4 @@ let detalle = sessionStorage.getItem('detalleCompra');
 console.log(detalle); // 'Libro de JavaScript'
 // Eliminar datos al cerrar la pestaña
 sessionStorage.removeItem('detalleCompra');
+

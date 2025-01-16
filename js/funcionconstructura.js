20c3ceb9b8a586a94defd7f4ef429f1ef6f17319
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2025-01-16 16:21:45 -0300

    Actualización Función Constructora

--- js/funcionconstructura.js
+++ js/funcionconstructura.js
@@ -1,13 +1,48 @@
-function Persona(nombre, edad){
-    this.nombre = nombre; 
-    this.edad = edad; 
+//funciones constructores: nos permiten crear objetos, su particularidad es que trabaja con el operador new
 
-    this.saludar = function(){
-        console.log(`Hola me llamo ${this.nombre} y tengo ${this.edad}`);  
+//Ejemplo de Uso Pre-ES6 ------------------
+// function Persona(nombre, edad, calle) {
+//     this.nombre = nombre;
+//     this.edad = edad;
+//     this.calle = calle;
+//     this.describir = function() {
+//         return `Nombre: ${this.nombre}, Edad: ${this.edad}, Calle: ${this.calle}`;
+//     };
+// }
+
+// // Crear un nuevo objeto Persona
+// var persona1 = new Persona("Ana", 25, "Calle Falsa 123");
+// console.log(persona1.describir());
+
+//Comparación con Clases en ES6--------------------
+class Persona {
+    constructor(nombre, edad, calle) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.calle = calle;
+    }
+
+    describir() {
+        return `Nombre: ${this.nombre}, Edad: ${this.edad}, Calle: ${this.calle}`;
     }
 }
-let persona1 =  new Persona("Juan", 33); 
-console.log(persona1.nombre); 
-console.log(persona1.edad)
 
-persona1.saludadar(); 
+// Crear un nuevo objeto Persona
+const persona1 = new Persona("Ana", 25, "Calle Falsa 123");
+console.log(persona1.describir());
+
+//funciones constructores: nos permiten crear objetos, su particularidad es que trabaja con el operador new
+function persona2(nombre, edad){
+    this.nombre= nombre; 
+    this.edad = edad; 
+    
+    this.saluda = function(){
+        console.log(`Hola me llamo ${this.nombre} y tengo ${this.edad}`)
+    }
+}
+
+let persona2 = new Persona ("Juan", 31)
+
+console.log(persona2.nombre); 
+console.log(persona2.edad)
+persona2.saluda(); 

56f2debefd6c54f507a4bfc3e918f132d397626c
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2025-01-20 15:01:06 -0300

    Nueva modificación Clase en vivo

--- js/clasevivo.js
+++ js/clasevivo.js
@@ -108,12 +108,103 @@ class Mascota{
     } 
 }
 
-const perrito = new Mascota("Perro", "Daisy", 4, "Yorki", "Beige", 2.5)
-const gatito = new Mascota("Gato", "Michi", 4, "Angora", "Blanco", 2.5)
+// const perrito = new Mascota("Perro", "Daisy", 4, "Yorki", "Beige", 2.5)
+// const gatito = new Mascota("Gato", "Michi", 4, "Angora", "Blanco", 2.5)
 
-console.log(perrito); 
-console.log(gatito); 
+// console.log(perrito); 
+// console.log(gatito); 
 
-perrito.comer(); 
-gatito.comer(); 
+// perrito.comer(); 
+// gatito.comer(); 
 
+// perrito.comer(); 
+// gatito.comer(); 
+
+// function caminar() {
+//     console.log("Mi " + perrito.tipo + " "+ perrito.nombre + " esta caminando.!")
+// }; 
+
+// caminar()
+
+//AlMACENAMIENTO 
+
+// let mensaje = "Hola desde Coder"; 
+// let mensaje = "Hola dessde Coderhouse"; 
+// localStorage.setItem('mensaje', mensaje); 
+
+// console.log(localStorage.getItem('mensaje')); 
+
+// localStorage.setItem('numero', 89); 
+// localStorage.getItem('boleno', true); 
+
+// localStorage.setItem('Perro', perrito); // No se va a ver por consola ya que solo guarda valores en formato string 
+// localStorage.setItem('Perro',JSON.stringify(perrito)); 
+// localStorage.setItem('Perro',JSON.stringify(Gatito)); 
+// const carrito = []; 
+// localStorage.setItem('carrito', carrito); 
+
+
+// console.log(typeof perrito)
+// console.log(typeof JSON.stringify(perrito))
+
+
+// const perritoComoSring = localStorage.getItem('Perro'); 
+// console.log(perritoComoSring); 
+
+// const perritoComoObject = JSON.parse(localStorage.getItem('Perro')); 
+// console.log(perritoComoObject); 
+
+// localStorage.removeItem('numero'); 
+// localStorage.removeItem('booleans'); 
+
+// localStorage.clear(); 
+
+// for(let i = 0; i <localStorage.length;  i++){
+//     let claver = localStoragekey(i); 
+//     console.log("Para la clave: " + clave + "\nValor" + localStorage.getItem(clave)); 
+// }
+
+// const guardarEnElLocalSorage = (clave, valor) =>{
+//     localStorage.setItem(clave,valor)
+// }
+
+// function Perro(nombre, edad, raza, color, peso){
+//     this.nombre = nombres; 
+//     this.edada = edad; 
+//     this.raza = raza; 
+//     this.color = color; 
+//     this.peso = peso; 
+// }
+
+// const perrito = new Perro ("Daisy", 4, "Yorki", "Biege", 2.5); 
+
+// guardarEnElLocalSorage("Perro", JSON.stringify(perrito)); 
+
+// guardarEnElLocalSorage(perrito.nombre, JSON.stringify(perrito)); 
+
+// localStorage.clear(); 
+
+
+const perrito = new Mascota("Perro", "Daisy", 4, "Yorki", "Beige", 2.5);
+const perrito1 = new Mascota("Perro", "Max", 6, "Collie", "Beige", 25);
+const gatito = new Mascota("Gato", "Michi", 4, "Gato", "Blanco", 3);
+const gatito1 = new Mascota("Gato", "Tom", 9, "Persa", "Negro", 5);
+
+const arrayDeMascotas = []
+
+arrayDeMascotas.push(perrito);
+arrayDeMascotas.push(perrito1, gatito, gatito1);
+
+console.log(arrayDeMascotas);
+
+for (const mascota of arrayDeMascotas){
+    guardarEnElLocalStorage(mascota.nombre, JSON.stringify(mascota));
+}
+
+for (const mascota of arrayDeMascotas){
+    guardarEnElLocalStorage('Mascota', JSON.stringify(mascota));
+}
+
+// for (const mascota of arrayDeMascotas){
+    guardarEnElLocalStorage('Mascotas', JSON.stringify(arrayDeMascotas));
+// }
